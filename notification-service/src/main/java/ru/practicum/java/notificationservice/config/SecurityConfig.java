7df5bd8c62038b994b8f35b5a166ecec9cb8e7f3
Roman Orinichev <roma-orinich@yandex.ru>
2025-06-13 01:20:05 +0200

    Created Oauth2.0 client credentials (m2m) clients; set up resource servers

---
+++ notification-service/src/main/java/ru/practicum/java/notificationservice/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package ru.practicum.java.notificationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().hasAuthority("ROLE_can-access-notification")
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> roles = Optional.ofNullable(jwt.getClaimAsMap("realm_access"))
+                    .map(map -> (List<String>) map.get("roles"))
+                    .orElse(Collections.emptyList());
+
+            List<GrantedAuthority> authorities = roles.stream()
+                    .map(role -> "ROLE_" + role)
+                    .peek(role -> System.out.println("Found role: " + role))
+                    .map(role -> (GrantedAuthority) new SimpleGrantedAuthority(role))
+                    .collect(Collectors.toList());
+
+            return authorities;
+        });
+
+        return converter;
+    }
+
+}
