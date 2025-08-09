cc7d49d99abcec4c68ab168034fe96013bd12830
Roman Orinichev <roma-orinich@yandex.ru>
2025-08-09 17:54:52 +0200

    Added placeholders to use later in configmaps and secrets; opened mistakenly closed health endpoint for front-ui service

--- front-ui/src/main/java/ru/practicum/java/frontui/config/SecurityConfig.java
+++ front-ui/src/main/java/ru/practicum/java/frontui/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login",
                                 "/process-login",
-                                "/actuator/health",
+                                "/actuator/**",
                                 "/home",
                                 "/register",
                                 "/profile/**").permitAll()
